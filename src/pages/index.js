f8f3541f0f9eace5eee33ce4e385fb1648e5fefa
Jarren San Jose <sanjose.jarren@gmail.com>
2020-06-24 23:07:22 -0700

    add fontawesome

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,34 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faPhotoVideo } from '@fortawesome/free-solid-svg-icons'
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>My name is Bash</h1>
+
     <p>I was born on February 8th, 2019.</p>
     <p>I currently live in the Bay Area!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <p><a href="https://instagram.com/kwajidog">Instagram</a></p>
-    <p><a href="https://photos.app.goo.gl/6CRM3G6QZUgsPgRp7">More photos of me</a></p>
+    <p>
+      <a href="https://instagram.com/kwajidog">
+        <FontAwesomeIcon icon={faInstagram} size="1x" />
+      </a>
+
+      <a href="https://instagram.com/kwajidog">
+        <FontAwesomeIcon icon={faYoutube} size="1x" style={{marginLeft: `1.45rem`}}/>
+      </a>
+
+      <a href="https://photos.app.goo.gl/6CRM3G6QZUgsPgRp7">
+        <FontAwesomeIcon icon={faPhotoVideo} size="1x" style={{marginLeft: `1.45rem`}}/>
+      </a>
+    </p>
   </Layout>
 )
 
