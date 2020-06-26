d4769f71839a29dcbe5a317a407f101d46c79090
Jarren San Jose <sanjose.jarren@gmail.com>
2020-06-25 21:33:16 -0700

    add social link

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faTiktok, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faTiktok, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faPhotoVideo } from '@fortawesome/free-solid-svg-icons'
 
 
@@ -24,11 +24,15 @@ const IndexPage = () => (
       <a href="https://instagram.com/kwajidog">
         <FontAwesomeIcon icon={faInstagram} size="1x" />
       </a>
-
+      
       <a href="https://www.youtube.com/channel/UCSgUrn2ojfTm7Fc_mS9Lbsw">
         <FontAwesomeIcon icon={faYoutube} size="1x" style={{marginLeft: `1.45rem`}}/>
       </a>
-      
+
+      <a href="https://www.twitch.tv/jarrensj">
+        <FontAwesomeIcon icon={faTwitch} size="1x" style={{marginLeft: `1.45rem`}}/>
+      </a>
+
       <a href="https://www.tiktok.com/@jarrensj">
         <FontAwesomeIcon icon={faTiktok} size="1x" style={{marginLeft: `1.45rem`}}/>
       </a>
